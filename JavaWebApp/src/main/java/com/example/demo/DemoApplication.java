a0008d2030b673f6676072efd501c58010f8bb71
nasanett <nasataku@yahoo.com>
2022-10-26 03:50:19 +0200

    fix

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -9,10 +9,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
+		String name = "vamsi";
 
-		string name ="nasa";
-
-		system.out.printIn(name);
+		System.out.println(name);
 	}
 
 }
